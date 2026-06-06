13e3cf6c33de654283868a5dbd67adc228cbc199
Gitudon <oniku.udon.029@gmail.com>
2026-06-06 21:48:04 +0900

    ABC460: A,B

---
+++ solves/ABC460/A.py
@@ -0,0 +1,8 @@
+N, M = map(int, input().split())
+ans = 0
+while M != 0:
+    x = N % M
+    M = x
+    ans += 1
+
+print(ans)
---
+++ solves/ABC460/B.py
@@ -0,0 +1,10 @@
+T = int(input())
+for _ in range(T):
+    X_1, Y_1, R_1, X_2, Y_2, R_2 = map(int, input().split())
+    if R_2 > R_1:
+        X_1, Y_1, R_1, X_2, Y_2, R_2 = X_2, Y_2, R_2, X_1, Y_1, R_1
+    d = (X_1 - X_2) ** 2 + (Y_1 - Y_2) ** 2
+    if (R_1 - R_2) ** 2 <= d <= (R_1 + R_2) ** 2:
+        print("Yes")
+    else:
+        print("No")
